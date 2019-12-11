83b0ae295ffa16a6783690e2a2ab8134d44a9d15
Un Known <jasonqan2004@gmail.com>
2019-12-11 18:55:17 +0000

    dArK mOdE

--- public/src/settings.js
+++ public/src/settings.js
@@ -1,10 +1,23 @@
 var settings = {};
 
 function updateSetting(name, value) {
+    if (value != settings[name].value) {
+        if (value) {
+            settings[name].setFunc();
+        } else {
+            settings[name].resFunc();
+        }
+    }
     settings[name].value = value;
+    localStorage.setItem(name, String(value));
 }
 
-function addSetting(name, text) {
+function addSetting(name, text, resetF, setF) {
+    settings[name] = {
+        value: false,
+        resFunc: resetF,
+        setFunc: setF
+    };
     var settingDiv = d3.select("#settings")
         .append("div")
         .classed("settings", true);
@@ -13,16 +26,21 @@ function addSetting(name, text) {
     var input = settingLabel.append("input")
         .attr("type", "checkbox")
         .on("change", function() {
-            updateSetting(name, this.value == "on");
+            updateSetting(name, this.checked);
         });
     settingLabel.append("span")
         .classed("slider round", true);
     settingDiv.append("p")
         .text(text);
-    settings[name] = {
-        value: false
-    };
+    if (localStorage.getItem(name) !== null) {
+        input.property("checked", localStorage.getItem(name) == "true");
+        input.dispatch("change");
+    }
 }
 
-addSetting("useicons", "Use icons");
-addSetting("dark", "dArK mOdE");
+addSetting("useicons", "Use icons", function(){}, function(){});
+addSetting("dark", "dArK mOdE", function () {
+    d3.select("body").classed("dark", false);
+}, function () {
+    d3.select("body").classed("dark", true);
+});
