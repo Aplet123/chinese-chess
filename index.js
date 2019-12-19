4ce70edb6ffe69f7f07fd600c4552da771864f24
Aplet123 <jasonqan2004@gmail.com>
2019-12-19 11:01:10 -0500

    start of a great thign

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 require("dotenv").config();
 const WebSocket = require("ws");
-const connection = require("./src/WSHandle.js");
 const app = express();
 const port = process.env.PORT || 9090;
+
+const BoardManager = require("./src/BoardManager.js");
+const bm = new BoardManager();
+const connection = require("./src/WSHandle.js").init(bm).connection;
+
+const expireTime = 1000 * 60 * 30;
 
 app.use(function (req, res, next) {
     // prevent caching for development purposes
@@ -15,7 +20,29 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.use(express.static("./public"));
+app.use("/game/:key", function (req, res) {
+    let key = req.params.key;
+    let board = bm.getBoard(key);
+    res.contentType("application/json");
+    if (key && board) {
+        res.send(JSON.stringify({
+            status: "Success"
+        }));
+    } else {
+        res.status(400);
+        res.send(JSON.stringify({
+            error: "Board not found"
+        }));
+    }
+});
+
+app.use(express.static("public"));
+
+app.use(function (req, res) {
+    res.status(404);
+    res.contentType("text/plain");
+    res.send("Sorry! Page not found!");
+});
 
 const server = app.listen(port);
 const wss = new WebSocket.Server({
@@ -29,6 +56,13 @@ setInterval(function() {
             c.ping();
         } else {
             c.terminate();
+        }
+    });
+    bm.boards.forEach(b => {
+        if (b.whitePlayer || b.blackPlayer) {
+            b.expires = Date.now() + expireTime;
+        } else if (Date.now() > b.expires) {
+            bm.deleteBoard(b);
         }
     });
 }, 15000);
--- src/WSHandle.js
+++ src/WSHandle.js
@@ -1,120 +1,115 @@
-const BoardManager = require("./BoardManager.js");
 const WebSocket = require("ws");
-const bm = new BoardManager();
 const serialize = require("./serialize.js");
-const expireTime = 1000 * 60 * 30;
 
-function connection (ws) {
-    function sendInstruction(ins, v) {
-        ws.send(JSON.stringify({
-            ins,
-            v
-        }));
-    }
-    let key;
-    ws.on("message", function (msg) {
-        if (ws.readyState == WebSocket.CLOSED || ws.readyState == WebSocket.CLOSING) {
-            return;
+function init (bm) {
+    function connection(ws) {
+        function sendInstruction(ins, v) {
+            ws.send(JSON.stringify({
+                ins,
+                v
+            }));
         }
-        let data;
-        try {
-            data = JSON.parse(msg);
-        } catch (err) {
-            return;
-        }
-        if (data.ins == "CREATE") {
-            sendInstruction("KEY", bm.createBoard());
-        } else if (data.ins == "JOIN") {
-            if (bm.isAvailable(data.v)) {
-                key = data.v;
-                bm.join(key, ws);
-                sendInstruction("JOINED", true);
-                bm.sendOther(key, "OP_JOINED");
-                sendInstruction("SIDE", bm.getSide(key));
-                if (bm.getBoard(key)) {
-                    sendInstruction("BOARD", serialize(bm.getBoard(key)));
-                }
-                sendInstruction("KEY", key);
-                sendInstruction("OTHERKEY", bm.getOtherKey(key));
-            } else {
-                sendInstruction("JOINED", false);
+
+        let key;
+        ws.on("message", function (msg) {
+            if (ws.readyState == WebSocket.CLOSED || ws.readyState == WebSocket.CLOSING) {
+                return;
             }
-        } else if (data.ins == "GETOTHERKEY") {
-            if (key) {
-                sendInstruction("OTHERKEY", bm.getOtherKey(key));
+            let data;
+            try {
+                data = JSON.parse(msg);
+            } catch (err) {
+                return;
             }
-        } else if (data.ins == "GETKEY") {
-            if (key) {
-                sendInstruction("KEY", key);
-            }
-        } else if (data.ins == "MOVE") {
-            if (key) {
-                if (data.v instanceof Array && data.v.length >= 4) {
-                    data.v = data.v.map(v => parseInt(v, 10));
-                    bm.move(key, data.v[0], data.v[1], data.v[2], data.v[3]);
+            if (data.ins == "CREATE") {
+                sendInstruction("KEY", bm.createBoard());
+            } else if (data.ins == "JOIN") {
+                if (bm.isAvailable(data.v)) {
+                    key = data.v;
+                    bm.join(key, ws);
+                    sendInstruction("JOINED", true);
+                    bm.sendOther(key, "OP_JOINED");
+                    sendInstruction("SIDE", bm.getSide(key));
                     if (bm.getBoard(key)) {
-                        bm.sendAll(key, "BOARD", serialize(bm.getBoard(key)));
+                        sendInstruction("BOARD", serialize(bm.getBoard(key)));
+                    }
+                    sendInstruction("KEY", key);
+                    sendInstruction("OTHERKEY", bm.getOtherKey(key));
+                } else {
+                    sendInstruction("JOINED", false);
+                }
+            } else if (data.ins == "GETOTHERKEY") {
+                if (key) {
+                    sendInstruction("OTHERKEY", bm.getOtherKey(key));
+                }
+            } else if (data.ins == "GETKEY") {
+                if (key) {
+                    sendInstruction("KEY", key);
+                }
+            } else if (data.ins == "MOVE") {
+                if (key) {
+                    if (data.v instanceof Array && data.v.length >= 4) {
+                        data.v = data.v.map(v => parseInt(v, 10));
+                        bm.move(key, data.v[0], data.v[1], data.v[2], data.v[3]);
+                        if (bm.getBoard(key)) {
+                            bm.sendAll(key, "BOARD", serialize(bm.getBoard(key)));
+                        }
                     }
                 }
-            }
-        } else if (data.ins == "SENDCHAT") {
-            if (key) {
-                if ((typeof data.v == "string") && data.v.length <= 200 && data.v.length > 0) {
-                    bm.sendAll(key, "CHAT",  {
-                        side: bm.getSide(key),
-                        message: data.v
-                    });
-                }
-            }
-        } else if (data.ins == "FORFEIT") {
-            if (key) {
-                let board = bm.getBoard(key);
-                let side = bm.getSide(key);
-                if (board && side) {
-                    bm.sendAll(key, "WINCON", side + "_forfeit");
-                    bm.deleteBoard(board);
-                }
-            }
-        } else if (data.ins == "DRAW") {
-            if (key) {
-                let board = bm.getBoard(key);
-                let side = bm.getSide(key);
-                if (board && side) {
-                    if (!(board.blackDraw || board.whiteDraw)) {
-                        bm.sendOther(key, "OP_DRAW");
+            } else if (data.ins == "SENDCHAT") {
+                if (key) {
+                    if ((typeof data.v == "string") && data.v.length <= 200 && data.v.length > 0) {
+                        bm.sendAll(key, "CHAT", {
+                            side: bm.getSide(key),
+                            message: data.v
+                        });
                     }
-                    if (side == "black") {
-                        board.blackDraw = true;
-                    } else {
-                        board.whiteDraw = true;
-                    }
-                    if (board.blackDraw && board.whiteDraw) {
-                        bm.sendAll(key, "WINCON", "both_draw");
+                }
+            } else if (data.ins == "FORFEIT") {
+                if (key) {
+                    let board = bm.getBoard(key);
+                    let side = bm.getSide(key);
+                    if (board && side) {
+                        bm.sendAll(key, "WINCON", side + "_forfeit");
                         bm.deleteBoard(board);
                     }
                 }
+            } else if (data.ins == "DRAW") {
+                if (key) {
+                    let board = bm.getBoard(key);
+                    let side = bm.getSide(key);
+                    if (board && side) {
+                        if (!(board.blackDraw || board.whiteDraw)) {
+                            bm.sendOther(key, "OP_DRAW");
+                        }
+                        if (side == "black") {
+                            board.blackDraw = true;
+                        } else {
+                            board.whiteDraw = true;
+                        }
+                        if (board.blackDraw && board.whiteDraw) {
+                            bm.sendAll(key, "WINCON", "both_draw");
+                        }
+                    }
+                }
             }
-        }
-    });
-    ws.on("close", function() {
-        bm.sendOther(key, "OP_DC");
-        bm.leave(key);
-    });
-    // close broken connections
-    ws.alive = true;
-    ws.on("pong", function() {
-        this.alive =  true;
-    });
+        });
+        ws.on("close", function () {
+            bm.sendOther(key, "OP_DC");
+            bm.leave(key);
+        });
+        // close broken connections
+        ws.alive = true;
+        ws.on("pong", function () {
+            this.alive = true;
+        });
+    }
+
+    return {
+        connection
+    };
 }
 
-setInterval(function () {
-    bm.boards.forEach(b => {
-        if (b.whitePlayer || b.blackPlayer) {
-            b.expires = Date.now() + expireTime;
-        } else if (Date.now() > b.expires) {
-            bm.deleteBoard(b);
-        }
-    });
-}, 10000);
-
-module.exports = connection;
+module.exports = {
+    init
+};
